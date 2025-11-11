80a6c68131d84d5e696f123915a48376f83f993e
titoo9201 <titoos675@gmail.com>
2025-11-11 23:46:51 +0530

    50% done work

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 function connectDB() {
-    mongoose.connect(process.env.MONGODB_URI)
+    mongoose.connect(process.env.MONGODB_URL)
         .then(() => {
             console.log("MongoDB connected");
         })
